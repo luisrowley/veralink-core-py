ddbcf3b1325846dcb8266410d233a65689041bca
luisrowley <louisdebenito@gmail.com>
2022-03-01 13:07:50 +0100

    added cose_key as constant value

--- encoder.py
+++ encoder.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 import cbor2
-from binascii import hexlify, unhexlify
-from shared.constants import ENCODING_SCHEMA
+from binascii import hexlify
+from shared.constants import COSE_KEY, ENCODING_SCHEMA
 from base45 import b45encode
-from cose.messages import Sign1Message
+from cose.messages import Sign1Message, CoseMessage
 from cose.algorithms import EdDSA
-from cose.keys.curves import Ed25519
 from cose.headers import Algorithm, KID
-from cose.keys.keyparam import KpKty, OKPKpD, OKPKpX, KpKeyOps, OKPKpCurve
-from cose.keys.keyops import SignOp, VerifyOp
 from cose.keys.cosekey import CoseKey
-from cose.keys.keytype import KtyOKP
+
 class Encoder():
     def __init__(self, schema=ENCODING_SCHEMA):
         self.schema = schema
@@ -25,19 +22,19 @@ class Encoder():
             payload = cbor2.dumps(payload))
         print(msg)
         
-        cose_key = {
-            KpKty: KtyOKP,
-            OKPKpCurve: Ed25519,
-            KpKeyOps: [SignOp, VerifyOp],
-            OKPKpD: unhexlify(b'9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60'),
-            OKPKpX: unhexlify(b'd75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a')}
-
+        cose_key = COSE_KEY
         cose_key = CoseKey.from_dict(cose_key)
         print(cose_key)
         msg.key = cose_key
         # encode function performs the signing automatically
         encoded = msg.encode()
         print(hexlify(encoded))
+        # decode and verify the signature
+        decoded = CoseMessage.decode(encoded)
+        print(decoded)
+        decoded.key = cose_key
+        print(decoded.verify_signature())
+        print(cbor2.loads(decoded.payload))
         return encoded
 
     """
--- shared/constants.py
+++ shared/constants.py
@@ -1,6 +1,19 @@
 from typing import Final
+from cose.keys.keyparam import KpKty, OKPKpD, OKPKpX, KpKeyOps, OKPKpCurve
+from cose.keys.keyops import SignOp, VerifyOp
+from cose.keys.keytype import KtyOKP
+from cose.keys.curves import Ed25519
+from binascii import unhexlify
 
 ENCODING_SCHEMA: Final = 'base45'
+COSE_KEY: Final = {
+    KpKty: KtyOKP,
+    OKPKpCurve: Ed25519,
+    KpKeyOps: [SignOp, VerifyOp],
+    OKPKpD: unhexlify(b'9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60'),
+    OKPKpX: unhexlify(b'd75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a')
+}
+
 CBOR_PAYLOAD = {
   "cn": "Wexton Inc",
   "sn": 1684671300,
