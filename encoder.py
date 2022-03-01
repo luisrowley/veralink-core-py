d781a0129c92d0893e4d601280021c8369118663
luisrowley <louisdebenito@gmail.com>
2022-03-01 14:09:35 +0100

    updated sign function and test keys

--- encoder.py
+++ encoder.py
@@ -1,44 +1,37 @@
 # -*- coding: utf-8 -*-
 import cbor2
-from binascii import hexlify
 from shared.constants import COSE_KEY, ENCODING_SCHEMA
 from base45 import b45encode
-from cose.messages import Sign1Message, CoseMessage
+from cose.messages import Sign1Message
 from cose.algorithms import EdDSA
 from cose.headers import Algorithm, KID
-from cose.keys.cosekey import CoseKey
 
 class Encoder():
     def __init__(self, schema=ENCODING_SCHEMA):
         self.schema = schema
-        
+
     """
-    Preliminar report with overall data
+    Signature function for CBOR structured payload
     """
     def sign_cbor_data(self, payload):
+        # message to be signed where:
+        #   phdr = protected headers
+        #   phdr = unprotected headers
         msg = Sign1Message(
             phdr = {Algorithm: EdDSA},
             uhdr = {KID: b'kid2'},
             payload = cbor2.dumps(payload))
-        print(msg)
-        
+
+        # cose key from dict
         cose_key = COSE_KEY
-        cose_key = CoseKey.from_dict(cose_key)
-        print(cose_key)
         msg.key = cose_key
-        # encode function performs the signing automatically
+
+        # encode function for signing automatically
         encoded = msg.encode()
-        print(hexlify(encoded))
-        # decode and verify the signature
-        decoded = CoseMessage.decode(encoded)
-        print(decoded)
-        decoded.key = cose_key
-        print(decoded.verify_signature())
-        print(cbor2.loads(decoded.payload))
         return encoded
 
     """
-    Preliminar report with overall data
+    Base45 converter for compressed data payload
     """
     def base45_encode(self, bytes):
         return 
--- test/cli.test.py
+++ test/cli.test.py
@@ -4,9 +4,22 @@ import os, sys
 p = os.path.abspath('.')
 sys.path.insert(1, p)
 
-from shared.constants import CBOR_PAYLOAD
+from shared.constants import CBOR_PAYLOAD, COSE_KEY
 from encoder import Encoder
+from decoder import Decoder
+from cose.messages import CoseMessage
+from binascii import hexlify
+import cbor2
 
 if __name__ == "__main__":
     encoder = Encoder()
-    encoder.sign_cbor_data(CBOR_PAYLOAD)
+    decoder = Decoder()
+    signed_data = encoder.sign_cbor_data(CBOR_PAYLOAD)
+    print('signed', hexlify(signed_data))
+    # decode and verify the signature
+    cose_key = COSE_KEY
+    decoded = CoseMessage.decode(signed_data)
+    print('decoded', decoded)
+    decoded.key = cose_key
+    print(decoded.verify_signature())
+    print(cbor2.loads(decoded.payload))
