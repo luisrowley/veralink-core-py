d2c4d9d0f796eef59f08dcdf9f1ca52ee6ed829d
luisrowley <louisdebenito@gmail.com>
2022-03-01 12:32:13 +0100

    changed encoding algorithm and keyparams

--- encoder.py
+++ encoder.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 import cbor2
+from binascii import hexlify, unhexlify
 from shared.constants import ENCODING_SCHEMA
 from base45 import b45encode
 from cose.messages import Sign1Message
-from cose.algorithms import Es256
+from cose.algorithms import EdDSA
+from cose.keys.curves import Ed25519
 from cose.headers import Algorithm, KID
+from cose.keys.keyparam import KpKty, OKPKpD, OKPKpX, KpKeyOps, OKPKpCurve
+from cose.keys.keyops import SignOp, VerifyOp
+from cose.keys.cosekey import CoseKey
+from cose.keys.keytype import KtyOKP
 class Encoder():
     def __init__(self, schema=ENCODING_SCHEMA):
         self.schema = schema
@@ -14,13 +20,25 @@ class Encoder():
     """
     def sign_cbor_data(self, payload):
         msg = Sign1Message(
-            phdr = {Algorithm: Es256},
-            uhdr = {KID: b'\x18\xc1L\x06\xedQ\x94('},
+            phdr = {Algorithm: EdDSA},
+            uhdr = {KID: b'kid2'},
             payload = cbor2.dumps(payload))
-        cose_key = None
-        msg.key = cose_key
         print(msg)
-        return msg
+        
+        cose_key = {
+            KpKty: KtyOKP,
+            OKPKpCurve: Ed25519,
+            KpKeyOps: [SignOp, VerifyOp],
+            OKPKpD: unhexlify(b'9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60'),
+            OKPKpX: unhexlify(b'd75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a')}
+
+        cose_key = CoseKey.from_dict(cose_key)
+        print(cose_key)
+        msg.key = cose_key
+        # encode function performs the signing automatically
+        encoded = msg.encode()
+        print(hexlify(encoded))
+        return encoded
 
     """
     Preliminar report with overall data
